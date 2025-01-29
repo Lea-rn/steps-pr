4203292b6acedc92439d5a76192455643b3672a1
achrefmechergui <achreffcb123achref@gamil.com>
2025-01-29 22:11:27 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
+import { useState } from "react";
+import "./index.css";
 
-import { useState } from 'react';
-import './index.css'
-
- const messages = [
-  "Learn React 🕷️" , 
-  "Apply for job 🏢" , 
-  "Invest your new income 🤑"
- ]
+const messages = [
+  "Learn React 🕷️",
+  "Apply for job 🏢",
+  "Invest your new income 🤑",
+];
 
 export default function App (){
-let [step,setStep] = useState(1)
+  return <>
+  <Steps/>
+  <Steps/>
+  </>
+}
 
 
+ function Steps() {
+  let [step, setStep] = useState(1);
+  let [isOpen, setOpen] = useState(true);
 
-
-  function handlePrevious (){
-    
-    if (step > 1 )  setStep(step-1)
-
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((cur) => cur - 1);
+      setStep((cur) => cur - 1)
+      // setStep(step-1)
+      // setStep(step-1)
+    }
   }
 
-  function handleNext (){
-   if(step < 3 ) setStep(step+1)
- 
+  function handleNext() {
+    if (step < 3) setStep((cur) => cur + 1);
   }
   return (
-
-  
-<>
-<h3 className='close'>✖️</h3>
-      <div className="step-container">
-       <div className="steps-number">
-          <h5 className={step>=1 ? "active" : ""}>1</h5>
-          <h5 className={step>=2 ? "active" : ""}>2</h5>
-          <h5 className={step>=3 ? "active" : ""}>3</h5>
-       </div>
-       <h3>Step {step} : {messages[step-1]}  </h3>
-       <div className="button-container">
-        <button onClick={handlePrevious} >Previous</button>
-        <button onClick={handleNext}>Next</button>
-  
-       </div>
-      </div>
-</>
-  )
+    <>
+      <h3 onClick={() => setOpen((is) => setOpen(!is))} className="close">
+        ✖️
+      </h3>
+      {isOpen && (
+        <div className="step-container">
+          <div className="steps-number">
+            <h5 className={step >= 1 ? "active" : ""}>1</h5>
+            <h5 className={step >= 2 ? "active" : ""}>2</h5>
+            <h5 className={step >= 3 ? "active" : ""}>3</h5>
+          </div>
+          <h3>
+            Step {step} : {messages[step - 1]}{" "}
+          </h3>
+          <div className="button-container">
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
